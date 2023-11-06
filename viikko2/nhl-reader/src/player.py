2a04f06d26df50a4b0f6e9521e861db4c8dac109
tovento <92781588+tovento@users.noreply.github.com>
2023-11-06 18:37:34 +0200

    Week2: Make "nhl-reader" list Finnish players

--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -1,6 +1,14 @@
 class Player:
     def __init__(self, dict):
         self.name = dict['name']
+        self.team = dict['team']
+        self.goals = dict['goals']
+        self.assists = dict['assists']
     
     def __str__(self):
-        return self.name
+        return (
+            f"{self.name} "
+            f"team {self.team} "
+            f"goals {self.goals} "
+            f"assists {self.assists}"
+        )
